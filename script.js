a2482a730bac802c5e09205a65f4580e67304113
MahakSolanki01 <168813284+MahakSolanki01@users.noreply.github.com>
2025-10-04 12:37:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector('.menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () =>{
+    navbar.classList.toggle('active');
+});
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+};
